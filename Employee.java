602879bafcea5e8255cee382162dd1cb60443054
Ivan <ikduong100@student.hbuhsd.edu>
2025-03-31 18:15:18 +0000

    class

--- Employee.java
+++ Employee.java
@@ -2,3 +2,26 @@
  *  Person class.  An employee has a jobTitle,
  *  and a salary.
  */
+
+public class Employee extends Person {
+    private String jobTitle;
+    private int salary;
+
+    public Employee(String name, int age, String jobTitle, int salary){
+        super(name, age);           //Person(String, int)
+        this.jobTitle = jobTitle;
+        this.salary = salary;
+    }
+
+    public Employee(String jobTitle, int salary){
+        super();
+        this.jobTitle = jobTitle;
+        this.salary = salary;
+    }
+
+    public Employee(){
+        super();
+        this.jobTitle = "Unemployed";
+        this.salary = 0;
+    }
+}
